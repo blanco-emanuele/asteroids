8130975a843207dc1d78e966fd303f34bc2913a7
blanco-emanuele <blanco.emanuele96@gmail.com>
2026-01-04 09:49:04 +0100

    added main player to the screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from logger import log_state
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -14,10 +15,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        
+        x = SCREEN_WIDTH / 2
+        y = SCREEN_HEIGHT / 2
+        player = Player(x, y)
+        player.draw(screen)
+
         pygame.display.flip()
         elapsed_milliseconds = clock.tick(60)
         dt = elapsed_milliseconds / 1000
-        # print(dt)
 
 if __name__ == "__main__":
     main()
