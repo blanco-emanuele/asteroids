41ead89e84fb51e76ee2c1a3bb62de0e3b8aab40
blanco-emanuele <blanco.emanuele96@gmail.com>
2026-01-04 23:25:28 +0100

    added bullets

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from constants import *
 from circleshape import *
+from shot import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -30,9 +31,18 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
         
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    def shoot(self):
+        bullet = Shot(self.position.x, self.position.y)
+        unit_vector = pygame.Vector2(0,1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SHOOT_SPEED
+        bullet.velocity = rotated_with_speed_vector
